c855d0831a13e01e5c6ba392385e5f5b119d4ba3
Luciana Silva <luciana.silva@qwasolucoes.com.br>
2022-05-23 15:26:27 -0300

    Conhecendo Predicate

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Products;
+import util.ProductPredicate;
 
 public class Program {
-	
-	public static int compareProducts(Products p1, Products p2) {
-		return p1.getPrice().compareTo(p2.getPrice());
-	}
 
 	public static void main(String[] args) {
 		
 		List<Products> list = new ArrayList<>();
 		
-		list.add(new Products("TV", 900.00));
-		list.add(new Products("Notebook", 1200.00));
-		list.add(new Products("Tablet", 450.00));
+		list.add(new Products("Tv", 900.00));
+		list.add(new Products("Mouse", 50.00));
+		list.add(new Products("Tablet", 350.50));
+		list.add(new Products("HD Case", 80.90));
 		
-		list.sort(Program::compareProducts);
-		list.forEach(System.out::println);
-	
+		list.removeIf(new ProductPredicate());
+		
+		for (Products p : list) {
+			System.out.println(p);
+		}
 	}
 }
